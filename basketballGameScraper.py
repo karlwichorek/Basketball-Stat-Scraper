9c70f422e34a29b1a2b72b965b5b2e03942465cc
Simon Swanson <nomiswanson@gmail.com>
2015-05-22 17:14:08 -0500

    First commit

---
+++ basketballGameScraper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# basketball-reference.py
+# Simon Swanson
+
+from bs4 import BeautifulSoup
+import re
+from requests import get
+import sys
+
+def main():
+    
+    return 0
+
+if __name__ == 'main__':
+    main()
---
+++ basketballInjuryScraper.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+# basketball-reference.py
+# Simon Swanson
+
+from   bs4      import BeautifulSoup
+import csv
+import re
+from   requests import get
+import sys
+
+prefix = "http://www.prosportstransactions.com/basketball/Search/"
+
+def getLinks(base):
+    soup  = BeautifulSoup(get("{}{}".format(prefix, base)).text)
+    tags  = soup.find("table", class_=None).findAll("td", width=None)[1].select("a[href]")
+    links = [tag.get("href") for tag in tags]
+    links.insert(0, base)
+    return links
+
+def getData(link):
+    data = []
+    soup = BeautifulSoup(get("http://www.prosportstransactions.com/basketball/Search/{}".format(link)).text)
+    rows = soup.find("table", class_="datatable center").findAll("tr", align="left")
+    [data.append(rowParse(row)) for row in rows]
+    return data
+
+def rowParse(line):
+    row   = []
+    parts = line.findAll("td")
+    row.append(parts[0].text.strip())
+    row.append(parts[1].text.strip())
+    row.append(parts[2].text.strip()) if parts[2].text.strip() else row.append(parts[3].text.strip())
+    row.append(parts[4].text.strip())
+    return row
+
+def main():
+    links = getLinks("SearchResults.php?Player&Team&BeginDate&EndDate&InjuriesChkBx=yes&Submit=Search&start=0")
+    with open("injuries.csv", "w", newline="") as fp:
+        injury = csv.writer(fp, delimiter=";", quoting=csv.QUOTE_ALL)
+        injury.writerow(["Date", "Team", "Player", "Notes"])
+        for link in links:
+            data = getData(link)
+            injury.writerows(data)
+    return "Successfully written to injuries.csv"
+
+if __name__ == "__main__":
+    main()
